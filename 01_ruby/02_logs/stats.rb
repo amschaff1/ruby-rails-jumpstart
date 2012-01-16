5f7c5c03e06868d742e7ec0caf040e276f902448
Amy Schaffer <amschaff1@gmail.com>
2012-01-16 18:01:22 -0500

    unique users and pages counted in stats.rb

--- 01_ruby/02_logs/stats.rb
+++ 01_ruby/02_logs/stats.rb
@@ -11,8 +11,8 @@ firstSkipped = false					# has the first line been skipped
 unique_users = {}		                # a hash of unique users
 unique_pages = {}                		# a hash of unique pages
 most_active_day = {}             		# a hash to keep track of the amount of activity per day
-most_active_user = "unknown"            # someday, this will work
-most_active_page = "unknown"            # someday, this will work
+most_active_user = {}             		# a hash to keep track of the number of unique users
+most_active_page = {}		            # a hash to keep track of the amount of activity on each page
 
 open(filename).each do |m|              # loop over every line of the file
     if(!firstSkipped)
@@ -29,11 +29,23 @@ open(filename).each do |m|              # loop over every line of the file
   	  	unique_pages[values[2]] = values[2] # add the page to the hash
   	  end
   	  
-  	  if(most_active_day.has_key?(values[0])) # if the day already exists in most_active_days
+  	  if(most_active_day.has_key?(values[0])) # if the day already exists in most_active_day
   	  	most_active_day[values[0]] += 1		# add one to it's value
   	  else
   	  	most_active_day[values[0]] = 1		# otherwise add it to the hash and make the value one
   	  end	
+  	  
+  	  if(most_active_user.has_key?(values[1])) # if the user already exists in most_active_user
+  	  	most_active_user[values[1]] += 1		# add one to it's value
+  	  else
+  	  	most_active_user[values[1]] = 1		# otherwise add it to the hash and make the value one
+  	  end
+  	  
+  	  if(most_active_page.has_key?(values[2])) # if the page already exists in most_active_page
+  	  	most_active_page[values[2]] += 1		# add one to it's value
+  	  else
+  	  	most_active_page[values[2]] = 1		# otherwise add it to the hash and make the value one
+  	  end
 
       lines += 1                            # bump the counter
     end
@@ -43,6 +55,8 @@ puts "total lines: #{lines}"                  # output stats
 puts "unique users: #{unique_users.length}"   # get the number of unique users in unique_users
 puts "unique pages: #{unique_pages.length}"   # get the nubmer of unique pages in unique_pages
 puts "most active day: #{most_active_day.sort_by { |day, count| count }.last.first}"   
-# sort the most active day hash and get the key of the highest value
-puts "most active user: #{most_active_user}"  # someday, this will work
-puts "most active page: #{most_active_page}"  # someday, this will work
+# sort the most active day hash by the values and get the key with the highest value
+puts "most active user: #{most_active_user.sort_by{ |user, count| count}.last.first}"  
+# sort the most active user hash by the values and get the key with the highest value
+puts "most active page: #{most_active_page.sort_by{ |page, count| count}.last.first}"  
+# sort the most active page hash by the values and get the key with the highest value
