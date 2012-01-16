b3daf0bc4297140719f17b6e52da3cbabbf61cec
Amy Schaffer <amschaff1@gmail.com>
2012-01-16 11:43:37 -0500

    skips the first line of the log and counts the number of unique users

--- 01_ruby/02_logs/stats.rb
+++ 01_ruby/02_logs/stats.rb
@@ -7,23 +7,30 @@ unless filename                         # we can't work without a filename
 end
 
 lines = 0                               # a humble line counter
-unique_users = "unknown"                # someday, this will work
+firstSkipped = false					# has the first line been skipped
+unique_users = {}		                # a hash of unique users
 unique_pages = "unknown"                # someday, this will work
 most_active_day = "unknown"             # someday, this will work
 most_active_user = "unknown"            # someday, this will work
 most_active_page = "unknown"            # someday, this will work
 
 open(filename).each do |m|              # loop over every line of the file
-  m.chomp!                              # remove the trailing newline
-  values = m.split(",")                 # split comma-separated fields into a values array
-
-  # ...
-
-  lines += 1                            # bump the counter
+    if(!firstSkipped)
+        firstSkipped = true				# the first line has been skipped
+    else
+      m.chomp!                              # remove the trailing newline
+      values = m.split(",")                 # split comma-separated fields into a values array
+    
+      if(!unique_users.has_key?(values[1]))	# if the user is not in unique_users
+      	unique_users[values[1]] = values[1]	# add the user to the hash
+  	  end
+  	  
+      lines += 1                            # bump the counter
+    end
 end
 
 puts "total lines: #{lines}"                  # output stats
-puts "unique users: #{unique_users}"          # someday, this will work
+puts "unique users: #{unique_users.length.to_s}" # the number of unique users
 puts "unique pages: #{unique_pages}"          # someday, this will work
 puts "most active day: #{most_active_day}"    # someday, this will work
 puts "most active user: #{most_active_user}"  # someday, this will work
