6b71858dba78788282fbf55c298caa002104c2ae
Amy Schaffer <amschaff1@gmail.com>
2012-01-25 14:45:24 -0500

    Modified duck.rb to take the query as a command line argument, created duckdef.rb to only print the definition from duck-duck-go results

--- 01_ruby/03_web/duckdef.rb
+++ 01_ruby/03_web/duckdef.rb
@@ -14,9 +14,6 @@ BASE_URL = "http://api.duckduckgo.com/?format=json&pretty=1&q="     # remote API
 query_url = BASE_URL + URI.escape(query)                            # putting the 2 together
 
 
-puts " ======================================== "                   # fancy output
-puts "   #{query_url}"                                              # fancy output
-
 object = open(query_url) do |v|                                     # call the remote API
   input = v.read                                                    # read the full response
   #puts input                                                       # un-comment this to see the returned JSON magic
@@ -24,17 +21,7 @@ object = open(query_url) do |v|                                     # call the r
 end
 
 puts " ======================================== "                   # fancy output
-puts "   #{object['Heading']}"
-puts "     #{object['Abstract']}"
-puts "     #{object['AbstractURL']}"
-puts " ---------------------------------------- "
-
-object['RelatedTopics'].each do |rt|                                # processing sub-elements
-  puts
-  puts "   * #{rt['Text']}"
-  puts "     #{rt['FirstURL']}"
-end
-
+puts 
+puts "     #{object['Definition']}"
 puts                                                                # fancy output
-puts " ---------------------------------------- "
 puts " ======================================== "
